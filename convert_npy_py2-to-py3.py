7d4e641eddb0dcf65e2c306921fd50c5c4b61891
Stephane M. Gagne <stephane.gagne@bcm.ulaval.ca>
2024-03-31 09:41:22 -0400

    initial commit

---
+++ convert_npy_py2-to-py3.py
@@ -0,0 +1,23 @@
+'''
+Convert numpy python2 .npy to python3
+This allows to avoid the following UserWarning:
+    UserWarning: Reading `.npy` or `.npz` file required additional header parsing as it was created on Python 2.
+    Save the file again to speed up loading and avoid this warning.
+Verifies that the arrays are unchanged.
+
+Written by Stéphane Gagné, Université Laval, QC, Canada
+'''
+
+import numpy as np
+import sys
+
+def convert_npy(file_in, file_out):
+    orig = np.load(file_in)
+    np.save(file_out, orig)
+    print("Saved file as : ", file_out)
+    new = np.load(file_out)
+    print("New array is equal original array : ", np.array_equal(orig, new))
+
+file_in = sys.argv[1]
+file_out = sys.argv[2]
+convert_npy(file_in, file_out)
