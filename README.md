1440c845646c572ac6090dc7fd2084aa558bcb81
Stephane M. Gagne <stephane.gagne@bcm.ulaval.ca>
2024-03-31 10:04:11 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-My miscellaneous utilities
+# My miscellaneous utilities
+
+## Convert npy from python2 to python3
+Convert numpy python2 .npy to python3
+Take two arguments as input : input_filename output_filename
+Verifies that the arrays are unchanged.
+
+This allows to avoid the following UserWarning:
+    UserWarning: Reading `.npy` or `.npz` file required additional header parsing as it was created on Python 2.
+    Save the file again to speed up loading and avoid this warning.
--- convert_npy_py2-to-py3.py
+++ convert_npy_py2-to-py3.py
@@ -1,9 +1,13 @@
 '''
 Convert numpy python2 .npy to python3
+
+Take two arguments as input : input_filename output_filename
+
+Verifies that the arrays are unchanged.
+
 This allows to avoid the following UserWarning:
     UserWarning: Reading `.npy` or `.npz` file required additional header parsing as it was created on Python 2.
     Save the file again to speed up loading and avoid this warning.
-Verifies that the arrays are unchanged.
 
 Written by Stéphane Gagné, Université Laval, QC, Canada
 '''
